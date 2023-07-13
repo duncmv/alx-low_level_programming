fef0d0d5839c0d6bb1a5fc0c6c96f4b01be65895
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-07-13 03:11:45 -0700

    tasks

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - a function that prints size of various types
+ * Return: 0
+ */
+int main(char a, int b, long int c, long long int d, float e)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
+	return (0);
+}
