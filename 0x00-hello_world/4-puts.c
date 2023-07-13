9b4ce153edec649917b9baddba07ddabf6dd4a83
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-07-13 02:02:58 -0700

    tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 
 /* 
  * main - A program using puts
+ * Return : 0
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
