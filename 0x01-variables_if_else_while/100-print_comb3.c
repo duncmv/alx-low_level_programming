02b6c5ee421ae42fa32855ebf413a5a908384362
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-07-14 04:06:49 -0700

    Tasks

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n < 58)
+	{	int x = n + 1;
+
+		while (x < 58)
+		{	if (x != n)
+			{	putchar(n);
+				putchar(x);
+			}
+			if (n < 56)
+			{	putchar(44);
+				putchar(32);
+			}
+			x++;
+		}
+		n++;	
+	}
+	putchar(10);
+	return (0);
+}
