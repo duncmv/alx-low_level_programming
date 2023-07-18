393c2c6494a263cf54c08bc3dd0039f21cd6ea4c
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-07-18 04:06:15 -0700

    Tasks

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-	char n[] = "_putchar";
-	int i = 0;
-
-	while (n[i] != "\0")
-	{
-		_putchar(n[i]);
-		i++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t'(;
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
