ec1f2b9c9ecad6158c32240caab91ecb332c8414
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-07-18 10:28:46 -0700

    Tasks

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - first 50 fibonacci numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int fib = 2;
+	int x = 1;
+	int n = 1;
+	int tmp;
+
+	printf("1, 2, ");
+	while (n < 50)
+	{
+		tmp = fib;
+		fib += x;
+		x = tmp;
+		printf("%d, ", fib);
+		n++
+}
+
