44457ed8d649365fedef22161dfa1e248bfebce9
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-07-18 03:08:50 -0700

    Tasks

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,10 +1,37 @@
 #include "main.h"
 /**
+ * minutes -prints minutes in an hour
+ * @a: int arg for h1
+ * @b: int arg for h2
+ */
+void minutes(int a, int b)
+{
+	int m1, m2;
+
+	m1 = 48;
+	while (m1 < 54)
+	{
+		m2 = 48;
+		while (m2 < 58)
+		{
+			_putchar(a);
+			_putchar(b);
+			_putchar(':');
+			_putchar(m1);
+			_putchar(m2);
+			_putchar('\n');
+			m2++;
+		}
+		m1++;
+	}
+}
+
+/**
  * jack_bauer - prints every minute of the day
  */
 void jack_bauer(void)
 {
-	int h1, h2, m1, m2;
+	int h1, h2;
 
 	h1 = 48;
 	while (h1 < 51)
@@ -14,40 +41,16 @@ void jack_bauer(void)
 		{
 			while (h2 < 53)
 			{
-				m1 = 48;
-				while (m1 < 54)
-				{
-					m2 = 48;
-					while (m2 < 58)
-					{
-						_putchar(h1 + h2 + ':' + m1 + m2);
-						m2++;
-					}
-					m1++;
-				}
+				minutes(h1, h2);
 				h2++;
 			}
+
 		}
 		else
 		{
 			while (h2 < 58)
 			{
-				m1 = 48;
-				while (m1 < 54)
-				{
-					m2 = 48;
-					while (m2 < 58)
-					{
-						_putchar(h1);
-						_putchar(h2);
-						_putchar(':');
-						_putchar(m1);
-						_putchar(m2);
-						_putchar('\n');
-						m2++;
-					}
-					m1++;
-				}
+				minutes(h1, h2);
 				h2++;
 			}
 		}
