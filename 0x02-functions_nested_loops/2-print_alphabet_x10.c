ef697fd541b3c4a7de2b14ef8f2da8724cd8abd3
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-07-18 04:00:50 -0700

    Tasks

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - a function that prints the alphabet in lowercase ten times
+ * print_alphabet_x10 - prints the alphabet in lowercase ten times
  */
 void print_alphabet_x10(void)
 {
