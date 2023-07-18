4f343cbc7c0173d398363c622c5dc6f1d3b7e820
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-07-18 04:43:54 -0700

    Tasks

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * times_table - prints 9 times table starting wit zero
+ */
+void times_table(void)
+{
+	int x, y, z;
+
+	x = 0;
+	while (x < 10)
+	{
+		y = 0;
+		while (y < 10)
+		{
+			z = x * y;
+			if (z > 9)
+			{
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+			{
+				_putchar(z + '0');
+			}
+
+			if (y != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			y++;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
