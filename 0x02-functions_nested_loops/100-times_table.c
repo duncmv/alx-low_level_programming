944d0bd9258d5346efda83349064d5911d46d6ea
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-07-18 09:58:49 -0700

    Tasks

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,41 @@
 #include "main.h"
 /**
+ * 100s - if multiple is in hundreds
+ * @p: int type arg
+ */
+void 100s(int p)
+{
+	int a = p % 100;
+
+	_putchar(p / 100 + '0');
+	_putchar(a / 10 + '0');
+	_putchar(p % 10 + '0');
+}
+
+
+/**
+ * 10s - if multiple is in tens
+ * @p: int type arg
+ */
+void 10s(int p)
+{
+	_putchar(' ');
+	_putchar(p / 10 + '0');
+	_putchar(p % 10 + '0');
+}
+
+/**
+ * 1s - if multiple is in ones
+ * @p: int type arg
+ */
+void 1s(int p)
+{
+	_putchar(' ');
+	_putchar(' ');
+	_putchar(p + '0');
+}
+
+/**
  * print_times_table - prints @n times table
  * @n: int type arg
  */
@@ -18,23 +54,15 @@ void print_times_table(int n)
 				z = x * y;
 				if (z > 99)
 				{
-					int a = z % 100;
-
-					_putchar(z / 100 + '0');
-					_putchar(a / 10 + '0');
-					_putchar(z % 10 + '0');
+					100s(z);
 				}
 				else if (z > 9 && z < 100)
 				{
-					_putchar(' ');
-					_putchar(z / 10 + '0');
-					_putchar(z % 10 + '0');
+					10s(z);
 				}
 				else if (y != 0)
 				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(z + '0');
+					1s(z);
 				}
 				else
 				{
