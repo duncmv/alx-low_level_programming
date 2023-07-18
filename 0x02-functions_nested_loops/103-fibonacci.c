6238e02cdb1ae75a52047b728af0749704d5cbfa
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-07-18 11:19:22 -0700

    Tasks

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - sum of fib numbers less than 4,000,000
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	long x = 1;
+	long fib = 0;
+	long tmp;
+	long sum = 0;
+
+	while (fib < 4000000)
+	{
+		tmp = fib;
+		fib += x;
+		x = tmp;
+		sum = sum + fib - 1;
+	}
+}
