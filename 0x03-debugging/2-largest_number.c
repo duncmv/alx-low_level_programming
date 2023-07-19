2e2697fe995792fa5a5f31b3a5593ecdafadd1b6
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-07-19 08:23:20 -0700

    Tasks

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,15 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+    int largest = a;
 
-    if ((a > b && b > c) || (a > c && c > b))
-    {
-        largest = a;
-    }
-    else if ((b > a && a > c) || (b > c && c > a))
+    if (b > a)
     {
         largest = b;
     }
-    else
+    if (c > b)
     {
         largest = c;
     }
-
     return (largest);
 }
