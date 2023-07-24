a470d66005a04d6d343de3b45876a7276f19fb7f
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-07-24 05:29:01 -0700

    Tasks

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcpy - copies string from src to dest
+ * @dest: pointer to char
+ * @src: pointer to char
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src);
+{
+	int i, n;
+
+	for (i = 0; src[i] != '\0'; i++)
+		;
+
+	i++;
+
+	for (n = 0; n < i; n++)
+		dest[n] = src[n];
+
+	return (dest);
+}
