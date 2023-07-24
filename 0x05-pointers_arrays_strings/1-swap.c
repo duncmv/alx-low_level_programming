b33bc366ab014e4c90243b2ab02b6b7f9b548466
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-07-24 01:06:12 -0700

    Tasks

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - swaps values of two integers 
+ * @a: int pointer arg
+ * @b: int pointer arg
+ */
+void swap_int(int *a,int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
