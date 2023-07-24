8a83de28de33693814b66313034dbbf8c309bdb7
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-07-24 01:25:25 -0700

    Tasks

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - returns length of string
+ * @s - char type arg
+ *
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int n = 0;
+
+	char x;
+
+	while  (x != '\0')
+	{
+		x = *(s + n);
+		n++;
+	}
+}
