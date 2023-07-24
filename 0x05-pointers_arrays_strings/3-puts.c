aa327427b42ca9c2d8f32e0da3322a5dcb734f30
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-07-24 01:46:13 -0700

    Tasks

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _puts - prints a string_
+ * @str: char type arg
+ */
+int _puts(char *str)
+{
+	int n = 0;
+
+	char x;
+
+	while (x != '\0')
+	{
+		x = *(str + n);
+
+		if (x != '\0')
+		{
+			_putchar(x + '0');
+			n++;
+		}
+
+	}
+	_putchar('\n');
+}
