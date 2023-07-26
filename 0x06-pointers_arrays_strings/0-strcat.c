8cf8b840ebb41124e2e0321ad7cf20d28e3da1b4
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-07-26 08:57:48 -0700

    Edit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
- * _strncat - function that concancates two strings
+ * _strcat - function that concancates two strings
  * @dest: pointer to string
  * @src: pointer to string
- * @n: byte limit
  *
  * Return: pointer to resulting string
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
 	int i, c;
 
 	for (i = 0; dest[i] != '\0'; i++)
 		;
 
-	for (c = 0; c < n; c++)
+	for (c = 0; src[c] != 0; c++)
 	{
 		dest[i] = src[c];
 		i++;
