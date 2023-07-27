27e7a092d440c499a5aa2779f55898fc0db78e3c
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-07-27 03:00:32 -0700

    Advanced

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,3 +7,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *c);
 char *cap_string(char *c);
 char *leet(char *c);
+char *rot13(char *c);
+void print_number(int n);
+void print_digit(int n);
