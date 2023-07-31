83de3b2aa0a010a2c60e1819cf4698979781d273
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-07-31 03:12:09 -0700

    advanced

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,13 +8,13 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
-	int sum1 = 0;
-	int sum2 = 0;
+	long int sum1 = 0;
+	long int sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		sum1 += a[i * size + 1];
+		sum1 += a[i * size + i];
 		sum2 += a[i * size + (size - 1 - i)];
 	}
-	printf("%d, %d\n", sum1, sum2);
+	printf("%ld, %ld\n", sum1, sum2);
 }
