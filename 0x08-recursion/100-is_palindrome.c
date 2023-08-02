28948014221383d26c2d96722780bac0b02e7f24
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-08-02 06:09:23 -0700

    Tasks

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,13 +7,13 @@
  */
 int _strlen_recursion(char *s)
 {
-        int i = 0;
+	int i = 0;
 
-        if (*s == '\0')
-                return (0);
+	if (*s == '\0')
+		return (0);
 
-        i = 1 + _strlen_recursion(s + 1);
-        return (i);
+	i = 1 + _strlen_recursion(s + 1);
+	return (i);
 }
 /**
  * is_palindrome - checks string for palindrome
