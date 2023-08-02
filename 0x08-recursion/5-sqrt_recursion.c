96f66e145111853edba26af5d508ed199cccb2a9
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-08-02 02:40:04 -0700

    Tasks

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - returns the natural sqroot of a number
+ * @n: int type arg
+ *
+ * Return: sqrt of n, or -1 if no natural root
+ */
+int _sqrt_recursion(int n)
+{
+	getroot(n, 1);
+}
+/**
+ * getroot - help function
+ * @n: original number to find root
+ * @i: first possible root
+ *
+ * Return: root or -1 if no natural root
+ */
+int getroot(int n, int i)
+{
+	int sq;
+
+	sq = i * i;
+
+	if (sq == n)
+		return (i);
+	else if (sq < n)
+		return(getroot(n, i + 1));
+	else
+		return (-1);
+}
+
