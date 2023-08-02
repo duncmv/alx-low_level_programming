add91370d38a70d61572c4566e557792ac462797
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-08-02 01:35:44 -0700

    Tasks

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,19 +5,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-	int i;
-
 	if (*s == '\0')
-	{
 		return;
-	}
 
-
-	for (i = 0; s[i]; i++)
-		;
-
-	i--;
-
-	_putchar(*(s + i));
 	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
