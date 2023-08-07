46c5a6dc91001a3c7180b47864a8436383808b5d
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-08-07 03:15:24 -0700

    Tasks

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "mainh"
+#include <stdlib.h>
+/**
+ * free_grid - frees a 2D array
+ * @grid: pointer to 2D array
+ * @height: height of array
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = height - 1; i >= 0; i--)
+		free(grid[i]);
+	free(grid);
+}
