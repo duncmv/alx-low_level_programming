c65f8becd1334fdb49e950e62ced432b72064594
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-08-07 04:19:30 -0700

    Advanced

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * argstostr - concatenates all args in prog
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: pointer or NULL
+ */
+char *argstostr(int ac, char **av)
+{
+	char *p;
+	int i, j, n = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+		n += strlen(av[i]);
+
+	p = malloc((n + ac) * sizeof(char));
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0, n = 0; i < ac; i++, n++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++, n++)
+		       p[n] = av[i][j];
+		p[n] = '\n';
+	}
+	return (p);
+}
