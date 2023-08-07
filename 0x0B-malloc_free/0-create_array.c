da63e5179dfe9cfa4db1691c2d0e6448a9573d1c
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-08-07 01:01:07 -0700

    Tasks

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib>
+/**
+ * create_array - creates an array of chars
+ * @size: size of array
+ * @c: character to fill array
+ *
+ * Return: pointer to array or NULL
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		p = malloc(size * sizeof(char));
+
+		if (p == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			for (i = 0; i < size; i++)
+				*(p + i) = c;
+		}
+	}
+	return (p);
+}
