e56ad42ecd44278e1d7d9c129e5f44b7fdde7248
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-08-07 03:06:37 -0700

    Tasks

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,18 +9,26 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int (*p)[width], i, j;
+	int **p, i, j;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	p = malloc(height * width * sizeof(int));
+	p = malloc(height * sizeof(*p));
 
 	if (p == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
+		p[i] = malloc(width * sizeof(**p));
+		if (p[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(p[i]);
+			free(p);
+			return (NULL);
+		}
 		for (j = 0; j < width; j++)
 			p[i][j] = 0;
 	}
