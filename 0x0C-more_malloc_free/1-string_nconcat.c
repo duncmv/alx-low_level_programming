86aa10dcd10e9058bd9234c8ea2d8c9fc9c087e1
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-08-10 04:08:02 -0700

    Tasks

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,33 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * len - finds length of strings
- * @s1: pointer to first string
- * @s2: pointer to second string
- * @n: no of byte
- *
- * Return: length of new string
- */
-unsigned int len(char *s1, char *s2, unsigned int n)
-{
-	unsigned int x, len1, len2;
-
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-
-	if (n >= len2)
-		x = len1 + len2 + 1;
-	else
-		x = len1 + n + 1;
-
-	return (x);
-}
-/**
  * string_nconcat - function that concatenates two strings
  * @s1: pointer to first string
  * @s2: pointer to second string
@@ -38,7 +11,7 @@ unsigned int len(char *s1, char *s2, unsigned int n)
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, x;
+	int i, j, x;
 	char *p;
 
 	if (s1 == NULL)
@@ -46,12 +19,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	x = len(s1, s2, n);
-	p = malloc(x * sizeof(*p));
+	x = (int)strlen(s1);
+	p = malloc((x + n + 1) * sizeof(*p));
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0, j = 0; i < x; i++)
+	for (i = 0, j = 0; i < (x + n); i++)
 	{
 		if (i < strlen(s1))
 			p[i] = s1[i];
