56dbcae2a33825ee6888e223544409bc88a7b558
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-08-10 04:01:59 -0700

    Tasks

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,14 +3,16 @@
 /**
  * malloc_checked - a function that allocates memory with malloc
  * @b: unsigne int type arg
+ *
+ * Return: voi pointer to mem allocated
  */
 void *malloc_checked(unsigned int b)
 {
 	void *p;
-	
+
 	p = malloc(b);
 	if (p == NULL)
-		exit (98);
+		exit(98);
 
 	return (p);
 }
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * len - finds length of strings
+ * @s1: pointer to first string
+ * @s2: pointer to second string
+ * @n: no of byte
+ *
+ * Return: length of new string
+ */
+unsigned int len(char *s1, char *s2, unsigned int n)
+{
+	unsigned int x, len1, len2;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	if (n >= len2)
+		x = len1 + len2 + 1;
+	else
+		x = len1 + n + 1;
+
+	return (x);
+}
+/**
+ * string_nconcat - function that concatenates two strings
+ * @s1: pointer to first string
+ * @s2: pointer to second string
+ * @n: no of bytes from s2 to be concatenated
+ *
+ * Return: pointer to new string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, x;
+	char *p;
+
+	x = len(s1, s2, n);
+	p = malloc(x * sizeof(*p));
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0, j = 0; i < x; i++)
+	{
+		if (i < strlen(s1))
+			p[i] = s1[i];
+		else
+			p[i] = s2[j++];
+	}
+	p[i] = '\0';
+
+	return (p);
+}
