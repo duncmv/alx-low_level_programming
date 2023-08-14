a674f5873c4968d8d1daf5fc338da9c0add16fd8
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-08-13 23:53:39 -0700

    Tasks

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,2 +1,4 @@
-#pragma once
+#ifndef 3_FUNCTION_LIKE_MACRO_H
+#define 3_FUNCTION_LIKE_MACRO_H
 #define ABS(x) ((x) < 0 ? (x) * -1 : (x))
+#endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,2 +1,4 @@
-#pragma once
+#ifndef 4_SUM_H
+#define 4_SUM_H
 #define SUM(x, y) ((x) + (y))
+#endif
