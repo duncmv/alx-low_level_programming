e4c649115d56cd62b46ff69d0c801db0e9dadb7f
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-08-14 02:20:17 -0700

    Tasks

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,9 +13,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (p == NULL)
 		return (NULL);
-	p->name = *name;
+	p->name = name;
 	p->age = age;
-	p->owner = *owner;
+	p->owner = owner;
 
 	return (p);
 }
