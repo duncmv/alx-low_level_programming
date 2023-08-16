ac78b0f49a5c9a12e863ee5c7e9f129287709ce3
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-08-16 03:02:58 -0700

    Tasks

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,7 +2,7 @@
 #ifndef FUNC_POINT_H
 #define FUNC_POINT_H
 void print_name(char *name, void (*f)(char *));
-int _putchar(char c)
+int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 #endif
