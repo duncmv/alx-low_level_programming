fcfa1df9f1d6937ae0857d2dfa3aa0da57246316
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-08-17 02:29:06 -0700

    Tasks

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+int _putchar(char c);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
@@ -11,6 +12,11 @@ void pchar(va_list list);
 void pstring(va_list list);
 void pint(va_list list);
 void pfloat(va_list list);
+/**
+ * struct form - struct to be used in print_all fun
+ * @type: specifies the datatype to be printed
+ * @f: pointer to func for corresponding type
+ */
 typedef struct form
 {
 	char *type;
