f4b28a1ddd481e474c6113198c550547400737db
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-08-30 06:20:57 -0700

    Tasks

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,33 +11,32 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 	listint_t *index, *ins = NULL;
 
-	if (*head)
+	index = *head;
+	ins = malloc(sizeof(listint_t));
+	if (!ins)
+		return (NULL);
+
+	if (idx != 0)
 	{
-		index = *head;
-		ins = malloc(sizeof(listint_t));
-		if (!ins)
-			return (NULL);
-		if (idx != 0)
+		for (i = 0; i < (idx - 1); i++)
 		{
-			for (i = 0; i < (idx - 1); i++)
+			index = index->next;
+			if (!index)
 			{
-				index = index->next;
-				if (!index)
-				{
-					free(ins);
-					return (NULL);
-				}
+				free(ins);
+				return (NULL);
 			}
-			ins->n = n;
-			ins->next = index->next;
-			index->next = ins;
 		}
-		else if (idx == 0)
-		{
-			ins->n = n;
-			ins->next = *head;
-			*head = ins;
-		}
+		ins->n = n;
+		ins->next = index->next;
+		index->next = ins;
 	}
+	else if (idx == 0)
+	{
+		ins->n = n;
+		ins->next = *head;
+		*head = ins;
+	}
+
 	return (ins);
 }
