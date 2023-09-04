fc3d10dae3fa5020767edbc7eeaf59eb4d73660c
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-09-04 02:58:57 -0700

    Tasks

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -26,7 +26,7 @@ int main(int ac, char **av)
 		dprintf(2, "Error: Can't write to %s\n", av[2]), exit(99);
 	do {
 		statusi = read(f[0], buff, 1024);
-		if (statusi = -1)
+		if (statusi == -1)
 		{
 			dprintf(2, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
