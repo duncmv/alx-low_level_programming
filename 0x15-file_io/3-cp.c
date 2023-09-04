fbccbdf4700f94308afa5c20a654e7d57b29a889
Duncan Tindiwensi Asiimwe <dtindiwensi@gmail.com>
2023-09-04 02:52:33 -0700

    Tasks

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,36 +11,40 @@
 int main(int ac, char **av)
 {
 	int f[2];
-	int status, s1, s2;
+	int statusi, statuso, s1, s2;
 	char buff[1024];
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (ac != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
+	
 	f[0] = open(av[1], 0);
 	if (f[0] == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-	f[1] = open(av[2], 1 | O_CREAT | O_TRUNC, 0664);
+		dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
+	f[1] = open(av[2], 1 | O_CREAT | O_TRUNC, mode);
 	if (f[1] == -1)
-	{
-		close(f[0]);
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
+		dprintf(2, "Error: Can't write to %s\n", av[2]), exit(99);
 	do {
-		status = read(f[0], buff, 1024);
-		write(f[1], buff, status);
+		statusi = read(f[0], buff, 1024);
+		if (statusi = -1)
+		{
+			dprintf(2, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
+		if (status > 0)
+		{
+			statuso =write(f[1], buff, status);
+			if (statuso == - 1)
+			{
+				dprintf(2, "Error: Can't write to %s\n", av[2]);
+				exit(99);
+			}
 	} while (status > 0);
 	s1 = close(f[1]);
 	s2 = close(f[0]);
 	if (s1 == -1 || s2 == -1)
 	{
-		dprintf(2, "Error Can't close fd %d\n", s1 == -1 ? f[1] : f[2]);
+		dprintf(2, "Error: Can't close fd %d\n", s1 == -1 ? 3 : 4);
 		exit(100);
 	}
 	return (0);
