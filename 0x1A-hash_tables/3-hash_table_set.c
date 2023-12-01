86e744e4fc01e0f0ffc178244c24506a2a842559
duncmv <dtindiwensi@gmail.com>
2023-12-01 10:50:06 +0300

    tasks

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,11 +9,29 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+	hash_node_t *new_node, *tmp;
 
 	if (new_node == NULL || key == NULL || strlen(key) == 0)
 		return (0);
+
 	index = key_index((const unsigned char *)key, ht->size);
+
+	tmp = ht->array[index];
+	if (tmp != NULL)
+	{
+		while (tmp)
+		{
+			if (strcmp(tmp->key, key) == 0)
+			{
+				free(tmp->value);
+				tmp->value = strdup(value);
+				return (1);
+			}
+			tmp = tmp->next;
+		}
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	new_node->next = ht->array[index];
